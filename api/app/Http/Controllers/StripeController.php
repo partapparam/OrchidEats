202a89c2046d6698b4e896969677ab74a5cc16f1
partapparam <paramsingh1961@gmail.com>
2018-02-08 08:50:28 -0800

    updates

---
+++ api/app/Http/Controllers/StripeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paramsingh
+ * Date: 2/8/18
+ * Time: 12:57 AM
+ */
+
+namespace OrchidEats\Http\Controllers;
+
+
+class StripeController
+{
+
+}
---
+++ api/app/Http/Requests/StripeTokenRequest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paramsingh
+ * Date: 2/8/18
+ * Time: 1:00 AM
+ */
+
+namespace OrchidEats\Http\Requests;
+
+
+class StripeTokenRequest
+{
+
+}

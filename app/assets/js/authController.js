5697ff0b0d2c0c6be36ca57d91bfdd5ac0e0b5f5
Hasibur Rahman Omi <hrahmanomi@gmail.com>
2018-02-08 17:54:07 +0600

    Price filtering done. Diet filtering isn't done yet

--- app/assets/js/authController.js
+++ app/assets/js/authController.js
@@ -15,7 +15,6 @@ angular.module('OrchidApp')
         function checkAuth() {
             if ($localStorage.token) {
                 $rootScope.auth = authService.getClaimsFromToken($localStorage.token);
-                console.log($rootScope.auth.data, $rootScope.auth);
             }
         }
 
